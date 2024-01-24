a525ee6c23698443036c5bfa6f306687c0b70552
Manoj <manojgowdacs35@gmail.com>
2024-01-24 23:12:04 +0530

    single step component

---
+++ wizard-form/components/Step.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Step = ({step}) => {
+    const {number,title} = step;
+    const currentStep = 1;
+  return (
+    <div className='flex flex-col md:flex-row items-center gap-3'>
+        <div className={`w-8  h-8  text-slate-50
+        border border-slate-50 rounded-full flex items-center
+        justify-center font-bold flex-shrink-0 ${number === currentStep ? 'bg-blue-300 border-0':''}`}>
+            {number}
+        </div>
+        <div className='flex flex-col  justify-center'>
+            <h4 className='text-slate-200 text-sm uppercase'>
+                Step {number}
+            </h4>
+            <h3 className='uppercase text-sm  text-white font-bold'>
+                {title}
+            </h3>
+        </div>
+    </div>
+  )
+}
+
+export default Step
