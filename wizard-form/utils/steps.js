5abe0657a14423bcdf1bfc89c3597dbd60b752d2
Manoj <manojgowdacs35@gmail.com>
2024-01-24 23:10:14 +0530

    steps list

---
+++ wizard-form/utils/steps.js
@@ -0,0 +1,28 @@
+const steps = [
+    {
+        number:1,
+        title:'Personal Info',
+    },
+    {
+        number:2,
+        title:'Education',
+    },
+    {
+        number:3,
+        title:'Programming Experience ',
+    },
+    {
+        number:4,
+        title:'Preferred Programming Languages',
+    },
+    {
+        number:5,
+        title:'Technical Skills Assessment',
+    },
+    {
+        number:6,
+        title:'Submit and Confirmation',
+    },
+]
+
+export default steps;
