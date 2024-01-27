a6e4d5e00c828756f92d178eeb9a8e362714392b
Manoj <manojgowdacs35@gmail.com>
2024-01-27 21:21:44 +0530

    confirmation form

--- wizard-form/components/StepForms/ConfirmForm.jsx
+++ wizard-form/components/StepForms/ConfirmForm.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import NavButton from '../FormInputs/NavButton';
 
 const ConfirmForm = () => {
+
+    const formData = useSelector((store)=>store.onboarding.formData)
+
+    async function processData(data){
+        console.log(data);
+    }
+
   return (
-    <div>ConfirmForm</div>
+    <div>
+        <form className='px-12 py-4' onSubmit={processData}>
+            <div className='mb-8'>
+                <h5 className='text-xl md:text-3xl font-bold text-gray-900 dark:text-white'>
+                    Confirm and Submit Data
+                </h5>
+                <p>Confirm if this the data you filled</p>
+            </div>
+            <div className='grid gap-2 sm:grid-cols-2'>
+                <code>
+                    <pre>{JSON.stringify(formData,null,2)}</pre>
+                </code>
+            </div>
+            <NavButton />
+        </form>
+    </div>
   )
 }
 
